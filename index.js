10d202cec1aa2aa523f704b908c64a731368cb19
nisentra <manuel.cano@ucp.edu.co>
2022-05-13 19:33:23 -0500

    victor cano :)

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ console.log ("Hola care-p..., soy jaime")
 console.log("Julian David Bernal Sossa - 1004733308");
 console.log('Aljandro colina 1088005963');
 <<<<<<< HEAD
-console.log ('victor manuel cano 1225090587');
-=======
+
 
 
 <<<<<<< HEAD
@@ -20,3 +19,15 @@ console.log("Hola Soy Jhonatan Giron Cabrera CC: 1004683958")
 console.log("ana mariaciro 1004682061")
 >>>>>>> 13fe9c4b443b2cab1c3fc1d569f47c77dc6ea79a
 >>>>>>> a02115d362782ce13058b5f9da374940adc866d5
+
+
+
+
+
+
+
+
+
+
+console.log ('victor manuel cano 1225090587');
+=======
