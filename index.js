f4cf564387cf127c7b149de027e02dcd1c0cf61e
carolina <dcas09@hotmail.com>
2022-05-13 19:24:51 -0500

    Jaime Dominguez

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  
 console.log ("Hola, soy Angy Melizza Castañeda Cano cc 1088323521")
 console.log("aca estuvo Christian todo bien!!!!")
-console.log ("Hola care-pussys, soy jaime")
+console.log ("Hola care-p..., soy jaime")
