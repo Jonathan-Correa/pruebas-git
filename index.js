2d19e3183cb59b38f170e3455bf08c1e9b8d76e1
felipe.clavijo <felipe.clavijo@ucp.edu.co>
2022-05-13 19:32:16 -0500

    pipeclavijo

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ console.log('Aljandro colina 1088005963');
 console.log('Aljandro colina 1088005963');
 
 
-<<<<<<< HEAD
 console.log("Hola Soy Jhonatan Giron Cabrera CC: 1004683958")
-=======
 
 
 console.log("ana mariaciro 1004682061")
->>>>>>> 13fe9c4b443b2cab1c3fc1d569f47c77dc6ea79a
