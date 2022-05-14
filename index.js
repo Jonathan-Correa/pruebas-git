bc06c71ec5c1e332e0ece07a3db08499c7da57ce
nisentra <manuel.cano@ucp.edu.co>
2022-05-13 19:29:45 -0500

    victor cano :)

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ console.log("aca estuvo Christian todo bien!!!!")
 
 console.log("Julian David Bernal Sossa - 1004733308");
 console.log('Aljandro colina 1088005963');
+console.log ('victor manuel cano 1225090587');
